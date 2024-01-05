649de6c4ddb360ea6181f0cec2a26f3bcd376d93
gonzalosanti <gsantiago_o@hotmail.com>
2024-01-05 16:13:03 -0600

    editando tabla cursousuario

--- api/v1.0/cursos/assignCurso.php
+++ api/v1.0/cursos/assignCurso.php
@@ -7,6 +7,7 @@ $assignTo = $_POST['assignTo'];
 
 if ($assignTo == "EMPRESA") {
   $delete = mysqli_query($mysqli, "delete from curso_empresa where id_empresa='$id'");
+  $inactive = mysqli_query($mysqli, "update curso_usuario set type_assign='USUARIO', id_assign='', status_course='0' where id_assign='$id'");
 
   if (!$delete) {
     $res = array(
@@ -21,6 +22,24 @@ if ($assignTo == "EMPRESA") {
 
   foreach ($courses as $el) {
     $insert = mysqli_query($mysqli, "insert into curso_empresa(id_curso, id_empresa) values ('$el','$id')");
+
+    $sql1 = "select id, id_empresa from usuarios where id_empresa='$id' ";
+    $result1 = $mysqli->query($sql1);
+    $usersEmpresa = [];
+    while ($row1 = $result1->fetch_assoc())   $usersEmpresa[] = $row1;
+
+    foreach ($usersEmpresa as $el2) {
+      $userEmpresa = $el2['id'];
+      $sql2 = "select id from curso_usuario where id_estudiante='$userEmpresa' AND id_curso='$el' ";
+    }
+
+    // $sqlcursos = "select curso_usuario.id, curso_usuario.id_curso, curso_usuario.id_estudiante from curso_usuario  inner join usuarios on usuarios.id_empresa='$id' inner join curso_usuario on curso_usuario.id_curso='$el'";
+    // $resultcursos = $mysqli->query($sqlcursos);
+    // $usersWCourse = [];
+    // while ($rowcursos = $resultcursos->fetch_assoc())   $usersWCourse[] = $rowcursos;
+
+    // // $exist=
+
 
     if (!$insert) {
       $res = array(
